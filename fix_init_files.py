174b27f85e2f531988ceab0fb8147629f401e566
NuniZ <nadav.nuni@cymmetria.com>
2018-06-05 14:38:29 +0300

    added argument support

--- fix_init_files.py
+++ fix_init_files.py
@@ -1,29 +1,39 @@
-import os, shutil
+import os
+import shutil
+import sys
 
-PATH_TO_GOOD_FILE = r"C:\Users\Nuni\PycharmProjects\mvp\server\DeceptionStation\decoymanager\manuf\test\__init__.py"
 GOOD_CONTENT = ['# -*- coding: utf-8 -*-\n', 'from __future__ import unicode_literals\n']
 
-def handle_file(path):
+def handle_file(path, path_to_good_file):
 	data = open(path, 'r').readlines()
 	if data == GOOD_CONTENT:
 		return False
 	if len(data) < 6:
 		print(data)
 		print (path, len(data))
+		# this is a "stop" that lets the user to decide if the file should be overridden
+		# you can change this part to your needs
 		change = raw_input()
 		if change == 'y':
 			os.remove(path)
-			shutil.copy(PATH_TO_GOOD_FILE, path)
+			shutil.copy(path_to_good_file, path)
 			return True
 	return False
 		
 
-changed = []
-for root, dirs, files in os.walk(os.getcwd()):
-	for filename in files:
-		path = os.path.join(root, filename)
-		if filename == "__init__.py":
-			is_changed = handle_file(path)
-			if is_changed:
-				changed.append(path)
-print(changed)
+def main(path_to_good_file):
+	changed = []
+	for root, dirs, files in os.walk(os.getcwd()):
+		for filename in files:
+			path = os.path.join(root, filename)
+			if filename == "__init__.py":
+				is_changed = handle_file(path, path_to_good_file)
+				if is_changed:
+					changed.append(path)
+	print(changed)
+	
+if __name__ == "__main__":
+	if len(sys.argv) != 2:	
+		print "Usage: {} <path_to_good_file>".format(sys.argv[0])
+		sys.exit(1)
+	main(sys.argv[1])
